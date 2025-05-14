79921d534ed0ea2e4198640aea4ce12737cb79f3
Krushant <krushantw@gmail.com>
2025-05-14 11:25:58 +0530

    changes in index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5173'],
+  origin: ['http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5173', 'https://mutual-fund-analyzer.vercel.app'],
   credentials: true
 }));
 app.use(express.json());
